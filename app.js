18fb677bd08e7d6680085358c02cef3c6871e213
Adarsha Shetty <adarsha.shetty.1989@gmail.com>
2017-01-21 18:45:26 +0530

    rolling back the port number to 3000

--- app.js
+++ app.js
@@ -6,13 +6,8 @@ var setup 	= require("./setup/index.js");
 var app = express();
 	app.use("/public/",express.static(__dirname + '/public'));
 
-
 setup(app);
-
-
-
-
 
 app.listen(config.PORT,function(){
 	console.log("App listening on port "+config.PORT);
-})
+});
