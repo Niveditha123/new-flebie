bc2ba5fb3e622a2df7141cf9fc787fed5a32965a
Adarsha Shetty <adarsha.shetty.1989@gmail.com>
2017-01-30 13:59:59 +0530

    Added gzip compression to all responses

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var compression = require("compression");
 var express = require("express");
 var config 	= require("./config/index.js");
 var setup 	= require("./setup/index.js");
@@ -6,11 +7,21 @@ var path = require('path');
 
 
 var app = express();
-	app.use("/public/",express.static(__dirname + '/public'));
-
+app.use("/public/",express.static(__dirname + '/public'));
+app.use(compression({filter: shouldCompress}));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+function shouldCompress (req, res) {
+	if (req.headers['x-no-compression']) {
+		// don't compress responses with this request header
+		return false;
+	}
+
+	// fallback to standard filter function
+	return compression.filter(req, res);
+}
 
 /**bodyParser.json(options)
  * Parses the text as JSON and exposes the resulting object on req.body.
