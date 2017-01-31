c04e1a81ede28da52ad7801ec378a9882800ec0f
Adarsha Shetty <adarsha.shetty.1989@gmail.com>
2017-01-31 21:42:33 +0530

    Made changes to enable https

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ var isProduction = process.env.NODE_ENV === 'production';
 
 setup(app);
 
-app.listen(config.PORT,function(){
+app.listen(443,function(){
 	console.log("App listening on port "+config.PORT);
 });
