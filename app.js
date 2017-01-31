addc6ffd10439532880c7557ea38939f1b9a0cf8
Adarsha Shetty <adarsha.shetty.1989@gmail.com>
2017-01-31 20:58:03 +0530

    Made changes for https

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express = require("express");
 var config 	= require("./config/index.js");
 var setup 	= require("./setup/index.js");
 var bodyParser = require("body-parser");
+var fs = require("fs");
 var path = require('path');
-
+var crypto = require('crypto');
+var node_cryptojs = require('node-cryptojs-aes');
 
 var app = express();
 app.use("/public/",express.static(__dirname + '/public'));
@@ -32,6 +34,12 @@ var isProduction = process.env.NODE_ENV === 'production';
 
 setup(app);
 
-app.listen(config.PORT,function(){
+/*app.listen(config.PORT,function(){
 	console.log("App listening on port "+config.PORT);
-});
+});*/
+
+require('https').createServer({
+	key: fs.readFileSync('certs/files.flebie.key'),
+	cert: fs.readFileSync('certs/7f2d4718b1db9ceb.crt'),
+	ca: [fs.readFileSync('certs/gd_bundle-g2-g1.crt')] // <----- note this part
+}, app).listen(config.PORT);
