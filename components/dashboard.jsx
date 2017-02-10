c6932cb3fa2293abeb720c01dc0f47b767ab02d1
Adarsha Shetty <adarsha.shetty.1989@gmail.com>
2017-02-10 08:36:14 +0530

    minor change in slot date

--- components/dashboard.jsx
+++ components/dashboard.jsx
@@ -183,7 +183,7 @@ class Dashboard extends React.Component {
                             <label> CHECK AVAILABILITY</label>
                         </div>
                         <div className="col-xs-3">
-                        <DatePicker id="slotDate" className="form-control" selected={this.state.slotDate} onChange={this.slotDateChange.bind(this)}  dateFormat="YYYY-MM-DD"/>
+                        <DatePicker id="slotDate" className="form-control" selected={this.state.slotDate} onChange={this.state.slotDateChange.bind(this)}  dateFormat="YYYY-MM-DD"/>
                         </div>
                         <div className="col-xs-5">
                             <TextareaAutosize readOnly  className="form-control" value={this.state.timeSlotArray.join("\n")} defaultValue={this.state.timeSlotArray.join("\n")} style={{ minHeight: 40, maxHeight: 200 }} />
