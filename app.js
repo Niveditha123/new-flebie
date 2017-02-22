4c9b22722b19a7df6ec15d872d285552fd3187c9
Adarsha Shetty <adarshashetty@Adarshas-MacBook-Pro-2.local>
2017-02-22 09:01:52 +0530

    Checking X-Forwarded-Proto header

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 var compression = require("compression");
+var fs = require("fs");
 var express = require("express");
-
+var https = require("https");
+var http = require("http");
 var config 	= require("./config/index.js");
 var setup 	= require("./setup/index.js");
 var bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser');
 var path = require('path');
-
-
+const httpsOptions = {
+	//cert: fs.readFileSync(path.join(__dirname, 'ssl','server.crt')),
+	//key:  fs.readFileSync(path.join(__dirname, 'ssl','server.key'))
+	//cert: fs.readFileSync(config.CERT_PATH),
+	//key:  fs.readFileSync(config.KEY_PATH) 
+};
 var app = express();
 app.use("/public/",express.static(__dirname + '/public'));
 app.use(compression({filter: shouldCompress}));
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 function shouldCompress (req, res) {
 	if (req.headers['x-no-compression']) {
 		// don't compress responses with this request header
@@ -32,9 +37,33 @@ function shouldCompress (req, res) {
 app.use(bodyParser.json());
 var isProduction = process.env.NODE_ENV === 'production';
 
-
+//redirect from http to https
+app.use(function(req, res, next) {
+	console.log("Request headers are: "+JSON.stringify(req.headers));
+	console.log("Request headers are: "+req.secure);
+	/*if((!req.secure)&& ((req.get('X-Forwarded-Proto') !== 'https'))) {
+		res.redirect('https://' + req.get('Host') + req.url);
+	}*/
+	/*if(!req.secure) {
+		return res.redirect(['https://', 'localhost:'+config.PORT, req.url].join(''));
+	}*/
+	next();
+});
 setup(app);
+//HTTPS server
+/*https.createServer(httpsOptions,app)
+	.listen(config.PORT,function(){
+		console.log("App listening on port "+config.PORT);
+	});*/
 
-app.listen(config.PORT,function(){
+//HTTP server
+/*http.createServer(app)
+	.listen(8082,function(){
+		console.log("App listening on port "+8082);
+	});	*/
+
+
+/*app.listen(config.PORT,function(){
 	console.log("App listening on port "+config.PORT);
 });
+*/
--- config/index.js
+++ config/index.js
@@ -5,5 +5,7 @@ module.exports = {
 	BCC_EMAIL: 'support@flebie.com',
 	SENDER: 'noreply@flebie.com',
 	MAILGUN_APIKEY: 'key-8fd25c849470876b918ec2c215f50c49',
-	MAILGUN_DOMAIN: 'mail.flebie.com'
+	MAILGUN_DOMAIN: 'mail.flebie.com',
+	CERT_PATH: '',
+	KEY_PATH: ''
 };
