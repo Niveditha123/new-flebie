9be7bacec194dd1aea54e7910f4e44c918adca59
Adarsha Shetty <adarshashetty@Adarshas-MacBook-Pro-2.local>
2017-02-26 21:27:09 +0530

    Changed cert config

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const httpsOptions = {
 	//cert: fs.readFileSync(config.CERT_PATH),
 	//key:  fs.readFileSync(config.KEY_PATH) 
 	//cert: fs.readFileSync('/home/ubuntu/csr_keys/server.crt'),
-	//key:  fs.readFileSync('/home/ubuntu/csr_keys/server.key'),
-	//csr:  fs.readFileSync('/home/ubuntu/csr_keys/server.csr'),
-	cert:   fs.readFileSync(path.join(__dirname, 'ssl','gd_bundle-g2-g1.crt')) 
+	key:  fs.readFileSync('/home/ubuntu/csr_keys/server.key'),
+	csr:  fs.readFileSync('/home/ubuntu/csr_keys/server.csr'),
+	//cert:   fs.readFileSync(path.join(__dirname, 'ssl','gd_bundle-g2-g1.crt')) 
 };
 var app = express();
 app.use("/public/",express.static(__dirname + '/public'));
