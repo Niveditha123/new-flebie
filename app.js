c2eac33e17197c0848311afd6c82c3a8a03acaed
Adarsha Shetty <adarshashetty@Adarshas-MacBook-Pro-2.local>
2017-02-27 19:26:20 +0530

    checking force ssl

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var isProduction = process.env.NODE_ENV === 'production';
 //redirect from http to https
 app.use(function(req, res, next) {
 	console.log("Port is: "+req.socket.localPort );
-	if(req.socket.localPort != 443)
+	if(req.socket.localPort != config.PORT)
 	{
 		res.redirect("https://www.flebie.com");
 	}
