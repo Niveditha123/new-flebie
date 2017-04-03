3910165bbbf40046c4152cd78d4ee8e0cfe1f3cb
Adarsha Shetty <adarsha89shetty@gmail.com>
2017-04-03 23:11:09 +0530

    checking port

--- app.js
+++ app.js
@@ -38,16 +38,18 @@ var isProduction = process.env.NODE_ENV === 'production';
 
 //redirect from http to https
 app.use(function(req, res, next) {
-	console.log("Port is: "+req.socket.localPort );
+	//console.log("Port is: "+req.socket.localPort );
 	if(req.socket.localPort != config.PORT)
 	{
+        console.log("In http section ");
+        console.log("Port is: "+req.socket.localPort );
         next();
 	}
 	else
 	{
-        console.log("Redirecting to https port");
-        res.redirect("https://www.flebie.com");
-
+        console.log("In https section ");
+        console.log("Port is: "+req.socket.localPort );
+        next();
 	}
 
 	
