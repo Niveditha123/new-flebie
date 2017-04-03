96d97e17151edfd47520cd0536f95022dfe73596
Adarsha Shetty <adarsha89shetty@gmail.com>
2017-04-03 22:38:18 +0530

    trying redirect to https

--- app.js
+++ app.js
@@ -39,15 +39,16 @@ var isProduction = process.env.NODE_ENV === 'production';
 //redirect from http to https
 app.use(function(req, res, next) {
 	console.log("Port is: "+req.socket.localPort );
-	/*if(req.socket.localPort != config.PORT)
+	if(req.socket.localPort != config.PORT)
 	{
+		console.log("Redirecting to https port");
 		res.redirect("https://www.flebie.com");
 	}
 	else
 	{
+        next();
+	}
 
-	}*/
-    next();
 	
 });
 setup(app);
